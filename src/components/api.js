b94133accda0f940791a6baccc46de6ec7258c45
solveigag <solveiga.g@outlook.com>
2022-08-23 22:53:30 +0100

    Add up and down vote to article

--- src/components/api.js
+++ src/components/api.js
@@ -1,31 +1,46 @@
+import axios from "axios";
+
 export const getAllArticles = (topic) => {
   if (topic) {
-    return fetch(`https://solveiga-nc-news-be.herokuapp.com/api/articles?topic=${topic}`).then(
-      (res) => {
-       return res.json();
-      }
-    );
+    return fetch(
+      `https://solveiga-nc-news-be.herokuapp.com/api/articles?topic=${topic}`
+    ).then((res) => {
+      return res.json();
+    });
   } else {
     return fetch("https://solveiga-nc-news-be.herokuapp.com/api/articles").then(
       (res) => {
         return res.json();
       }
     );
-  }   
-  };
-
-  export const getTopics = () => {
-    return fetch("https://solveiga-nc-news-be.herokuapp.com/api/topics").then(
-      (res) => {
-        return res.json();
-      }
-    );
   }
+};
 
-  export const getArticleById = (article_id) => {
-    return fetch(`https://solveiga-nc-news-be.herokuapp.com/api/articles/${article_id}`).then(
-      (res) => {
-        return res.json();
-      }
-    );
-  }
+export const getTopics = () => {
+  return fetch("https://solveiga-nc-news-be.herokuapp.com/api/topics").then(
+    (res) => {
+      return res.json();
+    }
+  );
+};
+
+export const getArticleById = (article_id) => {
+  return fetch(
+    `https://solveiga-nc-news-be.herokuapp.com/api/articles/${article_id}`
+  ).then((res) => {
+    return res.json();
+  });
+};
+
+export const patchVotes = (article_id, vote) => {
+  console.log(vote)
+  return axios
+    .patch(
+      `https://solveiga-nc-news-be.herokuapp.com/api/articles/${article_id}`,
+      { inc_votes: vote}
+    )
+    .then((res) => {
+      console.log(res)
+      return res;
+    });
+};
